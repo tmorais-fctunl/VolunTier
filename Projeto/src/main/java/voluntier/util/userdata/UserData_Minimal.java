fab03c11bdeaf038b574a7ea4e3731eedf7c18f2
MykeFr <myke.fr66@gmail.com>
2021-06-15 13:03:13 +0100

    small fix in username regex

--- Projeto/src/main/java/voluntier/util/userdata/UserData_Minimal.java
+++ Projeto/src/main/java/voluntier/util/userdata/UserData_Minimal.java
@@ -11,7 +11,7 @@ public class UserData_Minimal {
 	public static final String EMAIL_REGEX = ".+@.+[.].+";
 	public static final String POSTAL_CODE_REGEX = "[0-9]{4}-[0-9]{3}";
 	public static final String MOBILE_REGEX = "([+]351\\s)?[789][0-9]{8}";
-	public static final String USERNAME_REGEX = "^[a-zA-Z][a-zA-Z0-9][.]?[a-zA-Z0-9]?+$";
+	public static final String USERNAME_REGEX = "[a-zA-Z][a-zA-Z0-9]*([.][a-zA-Z0-9]+|[a-zA-Z0-9]*)";
 
 	public String username;
 	public String email;
