01fc13d2049551fe9763d4e91ca779d073d6a77d
tmorais-fctunl <t.morais@campus.fct.unl.pt>
2021-07-10 17:22:19 +0100

    Added CORS enabling with GCS

--- Projeto/src/main/java/voluntier/util/GoogleStorageUtil.java
+++ Projeto/src/main/java/voluntier/util/GoogleStorageUtil.java
@@ -1,6 +1,10 @@
 package voluntier.util;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.javatuples.Pair;
@@ -12,18 +16,46 @@ import com.google.cloud.storage.HttpMethod;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Cors;
+import com.google.common.collect.ImmutableList;
+
+
 public class GoogleStorageUtil {
+	
+	
+/*	public static void configureBucketCors(String bucketName, String origin, String responseHeader,
+			Integer maxAgeSeconds) {
+		//https://cloud.google.com/storage/docs/configuring-cors#storage_cors_configuration-java
+
+		Bucket bucket = storage.get(bucketName);
+		
+		Cors cors = Cors.newBuilder().setOrigins(ImmutableList.of(Cors.Origin.of(origin)))
+				.setMethods(ImmutableList.of(HttpMethod.GET, HttpMethod.PUT)).setResponseHeaders(ImmutableList.of(responseHeader))
+				.setMaxAgeSeconds(maxAgeSeconds).build();
+
+		bucket.toBuilder().setCors(ImmutableList.of(cors)).build().update();
+
+		System.out.println("Bucket " + bucketName + " was updated with a CORS config to allow GET requests from "
+				+ origin + " sharing " + responseHeader + " responses across origins");
+	}*/
+		
+	
+	
 	private static Storage storage = StorageOptions.getDefaultInstance().getService();
 	
 	private static Pair<URL, Long> common(String filename, HttpMethod method) {
+		
 		String bucketName = "voluntier-317915.appspot.com";
 		String fileName = filename;
 		BlobId blobId = BlobId.of(bucketName, fileName);
 		BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
+		
+		//configureBucketCors(bucketName, "https://voluntier-317915.appspot.com", "*", 15*60);
 
 		//long maxContentLength = 1048576; // 1Mb
 		//Map<String, String> extHeaders = new HashMap<>();
-		//extHeaders.put("x-goog-content-length-range", "0," + maxContentLength);
+		//extHeaders.put("Access-Control-Allow-Origin", "*");
 
 		URL signedURL = storage.signUrl(blobInfo, 15, TimeUnit.MINUTES,
 				Storage.SignUrlOption.httpMethod(method)/*, Storage.SignUrlOption.withExtHeaders(extHeaders)*/);
